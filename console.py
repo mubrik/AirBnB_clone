130b45ad600c61063072e3309b21a4381e9e4a31
mubrik <mubarakg4u@gmail.com>
2023-02-08 22:00:29 +0100

    style and logic changes

--- console.py
+++ console.py
@@ -16,11 +16,13 @@ class HBNBCommand(cmd.Cmd):
     Attributes:
     - prompt: A string to print as prompt
     """
-    prompt = '(hbnb) '
+
+    prompt = "(hbnb) "
 
     def preloop(self):
         """Set interpreter-wide attributes"""
-        self.available = {'BaseModel': BaseModel}
+        self.available = {"BaseModel": BaseModel}
+
     def emptyline(self):
         """Does nothing when the user enters an empty line"""
         return
@@ -38,16 +40,18 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, class_name):
-        """create <class_name>: Create and saves a new instance of <class_name>"""
+        """create <class_name>:
+        Create and saves a new instance of <class_name>"""
         # Verify class name
         if not class_name:
-            print('** class name missing **')
+            print("** class name missing **")
             return
-        elif class_name not in self.available:
+
+        if class_name not in self.available:
             print("** class doesn't exist **")
             return
 
-        obj = self.available[class_name]() # This looks ridiculous
+        obj = self.available[class_name]()  # This looks ridiculous
         storage.save()
         print(obj.id)
 
@@ -62,7 +66,7 @@ class HBNBCommand(cmd.Cmd):
         if args[0] not in self.available:
             print("** class doesn't exist **")
             return
-        elif len(args) == 1:
+        if len(args) == 1:
             print("** instance id missing **")
             return
         obj_name = f"{args[0]}.{args[1]}"
@@ -85,7 +89,7 @@ class HBNBCommand(cmd.Cmd):
         if args[0] not in self.available:
             print("** class doesn't exist **")
             return
-        elif len(args) == 1:
+        if len(args) == 1:
             print("** instance id missing **")
             return
         obj_name = f"{args[0]}.{args[1]}"
@@ -96,6 +100,7 @@ class HBNBCommand(cmd.Cmd):
 
         # instance exist, destroy it!
         del all_objs[obj_name]
+        storage.save()
 
     def do_all(self, line):
         """all [class_name]: Prints all the current saved objects."""
@@ -103,12 +108,17 @@ class HBNBCommand(cmd.Cmd):
         if line and line not in self.available:
             print("** class doesn't exist **")
             return
+        filter = True if line and line in self.available else False
         list_objs = []
-        for obj_name, obj in all_objs.items():
-            if not line:
-                list_objs.append(str(obj))
-            else:
-                list_objs.append(str(obj))
+        if filter:
+            list_objs = [v for k, v in all_objs.items() if line in k]
+        else:
+            list_objs = [v for _, v in all_objs.items()]
+        # for obj_name, obj in all_objs.items():
+        #     if not line:
+        #         list_objs.append(str(obj))
+        #     else:
+        #         list_objs.append(str(obj))
 
         print(list_objs)
 
@@ -119,10 +129,11 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         args = line.split()
+        arg_l = len(args)
         if args[0] not in self.available:
             print("** class doesn't exist")
             return
-        elif len(args) == 1:
+        if arg_l == 1:
             print("** instance id missing **")
             return
         obj_name = f"{args[0]}.{args[1]}"
@@ -130,32 +141,38 @@ class HBNBCommand(cmd.Cmd):
         if obj_name not in all_objs:
             print("** no instance found **")
             return
-        if len(args) == 2: # no attributes
+        if arg_l == 2:  # no attributes
             print("** attribute name missing **")
             return
-        elif len(args) == 3:
+        if arg_l == 3:
             print("** value missing **")
             return
-
+        print(args)
         # Figure out what the value is. Possible values are in integers,
         # float and strings.
-        value = args[3]
-        if value.count('.') == 1:  # possible float
+        key, val = args[2], args[3]
+        """ if val.count(".") == 1:  # possible float
             try:
-                value = float(value)
+                value = float(val)
             except ValueError:
-                pass # Not a float
-        elif value.isdigit():  # it is an int
-            value = int(value)
-
-# Have no access to the object here.
-        # Creating a new object with the same attributes as the current object
-        # and new attribute
-        obj_dict = all_objs[obj_name]
-        obj_dict[args[2]] = value
-        obj = self.available[args[0]](**obj_dict)
+                pass  # Not a float
+        elif val.isdigit():  # it is an int
+            value = int(val) """
+        value = None
+        try:
+            if "." in key:  # float
+                value = float(val)
+            else:
+                value = int(val)
+        except ValueError as e:
+            # value error means conversion failed, so str, can pass
+            value = str(val)
+        # Have no access to the object here. mubrik: we do now
+        instance = all_objs[obj_name]
+        instance[key] = value
+        instance.save()
+        # obj = self.available[args[0]](**obj_dict)
         # Overwrite the previous entry
-        obj.save()
 
 
 if __name__ == "__main__":
