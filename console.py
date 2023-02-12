c1bce0ac9a24edfa95b8c61a2ff27eb64a8ac753
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-02-12 20:59:07 +0100

    added support for update, destor

--- console.py
+++ console.py
@@ -27,6 +27,8 @@ class HBNBCommand(cmd.Cmd):
                 "all": self.do_all,
                 "count": self._count,
                 "show": self.do_show,
+                "destroy": self.do_destroy,
+                "update": self.do_update,
                 }
 
     def emptyline(self):
@@ -67,7 +69,7 @@ class HBNBCommand(cmd.Cmd):
             if not self._is_id_kwargs(kwargs):
                 return
             class_name = kwargs["class_name"]
-            ins_id = kwargs['options']
+            ins_id = self._get_id(kwargs)
         else:
             args = line.split()
             class_name = args[0]
@@ -81,14 +83,19 @@ class HBNBCommand(cmd.Cmd):
         # instance exist, print it!
         print(all_objs[obj_name])
 
-    def do_destroy(self, line: str):
+    def do_destroy(self, line: str, **kwargs):
         """destroy class_name object_id: destroy the object with the id
         object_id"""
         # Verify argument
-        if not self.is_line_valid(line, "destroy"):
+        if not kwargs and not self.is_line_valid(line, "destroy"):
             return
-        args = line.split()
-        obj_name = f"{args[0]}.{args[1]}"
+        elif kwargs and not self._is_id_kwargs(kwargs):
+            return
+        if line:
+            args = line.split()
+            obj_name = f"{args[0]}.{args[1]}"
+        else:
+            obj_name = f"{kwargs['class_name']}.{self._get_id(kwargs)}"
         all_objs = storage.all()
         if obj_name not in all_objs:
             print("** no instance found **")
@@ -114,54 +121,76 @@ class HBNBCommand(cmd.Cmd):
         # do we print an empty list?
         print(list_objs)
 
-    def do_update(self, line: str):
+    def do_update(self, line: str, **kwargs):
         """update class_name object_id attribute value: Updates the object with
         the id object_id by assigning the attribute <attribute> to <value>"""
-        # not limiting it splits the space in value eg:hello world
-        if not self.is_line_valid(line, "update"):
+        if not self._is_val_update(line, kwargs):
             return
-        args = line.split(" ", 3)
-        arg_l = len(args)
-        obj_name = f"{args[0]}.{args[1]}"
-        all_objs = storage.all()
-        if obj_name not in all_objs:
-            print("** no instance found **")
-            return
-        if arg_l == 2:  # no attributes
-            print("** attribute name missing **")
-            return
-        if arg_l == 3:
-            print("** value missing **")
-            return
-        key, val = args[2], args[3]
-        # do we strip here? if val = "33" ~= "'33'" the conversion to ..
-        # int below wont work?
-        value = None
-        try:
-            value = float(val) if "." in val else int(val)
-        except ValueError as e:
-            # just pass value?
-            value = self.get_update_str(val)
-        instance = all_objs[obj_name]
-        setattr(instance, key, value)
-        instance.save()
 
-    def get_update_str(self, line: str):
-        """Return the right string to use as value for the update command"""
-        # strip the first 3 arguments
-        """ for i in range(3):
-            idx = line.index(' ')
-            line = line[idx + 1:] """
-        # lets make a wild assumption
-        if line[0] == '"' and line[-1] == '"':
-            return line[1:-1]
-        if line[0] == "'" and line[-1] == "'":
-            return line[1:-1]
-        return line
-        # find the other quote
-        line = line[1:]
-        idx = line.index('"')
-        return line[:idx]
+        if line:
+            args = line.split(" ", 3)
+            obj_name = f"{args[0]}.{args[1]}"
+            self._set_attr(args[2], args[3], obj_name)
+            return
+        all_objs = storage.all()
+        if isinstance(kwargs['options'][1], str):
+            options = kwargs['options']
+            obj_name = f"{kwargs['class_name']}.{self._get_id(kwargs)}"
+            setattr(all_objs[obj_name], options[1], options[2])
+            return
+
+        obj_name = f"{kwargs['class_name']}.{self._get_id(kwargs)}"
+        for key, value in kwargs['options'][1].items():
+            setattr(all_objs[obj_name], key, value)
+
+    def do_User(self, line):
+        """User.command(options): Performs command on User with options"""
+        self.call_command('User', line)
+
+    def do_BaseModel(self, line):
+        """BaseModel.command(options): Performs command on BaseModel with options"""
+        self.call_command('BaseModel', line)
+
+    def do_Place(self, line):
+        """Place.command(options): Performs command on Place with options"""
+        self.call_command('Place', line)
+
+    def do_City(self, line):
+        """City.command(options): Performs command on City with options"""
+        self.call_command('City', line)
+
+    def do_Review(self, line):
+        """Review.command(options): Performs command on Review with options"""
+        self.call_command('Review', line)
+
+    def do_Amenity(self, line):
+        """Amenity.command(options): Performs command on Amenity with options"""
+        self.call_command('Amenity', line)
+
+    def do_State(self, line):
+        """State.command(options): Performs command on State with options"""
+        self.call_command('State', line)
+
+    def _count(self, line, **kwargs):
+        """Not a command!
+        Counts the number of instances of a class currently stored
+        called using the call_command.
+        Parameters:
+        - line: Not used
+        - kwargs: dictionary with:
+            - class_name: name of class to use
+            - options: not used
+        """
+        class_name = kwargs['class_name']
+
+        # count then print
+        all_objs = storage.all()
+        count = 0
+        for obj_name in all_objs:
+            if obj_name.startswith(class_name):
+                count += 1
+
+        print(count)
 
     def call_command(self, class_name, line):
         """Calls the right command to perform the task
@@ -195,33 +224,30 @@ class HBNBCommand(cmd.Cmd):
             return
         self.valid_commands[command](None, class_name=class_name, options=options)
 
-    def do_User(self, line):
-        """User.command(options): Performs command on User with options"""
-        self.call_command('User', line)
+    def get_update_str(self, line: str):
+        """Return the right string to use as value for the update command"""
+        # strip the first 3 arguments
+        """ for i in range(3):
+            idx = line.index(' ')
+            line = line[idx + 1:] """
+        # lets make a wild assumption
+        if line[0] == '"' and line[-1] == '"':
+            return line[1:-1]
+        if line[0] == "'" and line[-1] == "'":
+            return line[1:-1]
+        return line
+        # find the other quote
+        line = line[1:]
+        idx = line.index('"')
+        return line[:idx]
 
-    def do_BaseModel(self, line):
-        """BaseModel.command(options): Performs command on BaseModel with options"""
-        self.call_command('BaseModel', line)
-
-    def do_Place(self, line):
-        """Place.command(options): Performs command on Place with options"""
-        self.call_command('Place', line)
-
-    def do_City(self, line):
-        """City.command(options): Performs command on City with options"""
-        self.call_command('City', line)
-
-    def do_Review(self, line):
-        """Review.command(options): Performs command on Review with options"""
-        self.call_command('Review', line)
-
-    def do_Amenity(self, line):
-        """Amenity.command(options): Performs command on Amenity with options"""
-        self.call_command('Amenity', line)
-
-    def do_State(self, line):
-        """State.command(options): Performs command on State with options"""
-        self.call_command('State', line)
+    def _get_id(self, kwargs):
+        """Returns id in kwargs
+        """
+        if isinstance(kwargs["options"], tuple):
+            return kwargs['options'][0]
+        else:
+            return kwargs['options']
 
     def is_line_valid(self, line: str, func: str):
         """validator for all handlers """
@@ -243,27 +269,6 @@ class HBNBCommand(cmd.Cmd):
             return False
         return True
 
-    def _count(self, line, **kwargs):
-        """Not a command!
-        Counts the number of instances of a class currently stored
-        called using the call_command.
-        Parameters:
-        - line: Not used
-        - kwargs: dictionary with:
-            - class_name: name of class to use
-            - options: not used
-        """
-        class_name = kwargs['class_name']
-
-        # count then print
-        all_objs = storage.all()
-        count = 0
-        for obj_name in all_objs:
-            if obj_name.startswith(class_name):
-                count += 1
-
-        print(count)
-
     def _is_id_kwargs(self, kwargs):
         """Checks if the options passed is a valid id
         Parameter:
@@ -277,6 +282,15 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return False
 
+            # literal_eval can let other type pass and functions expect strings
+
+        # (obj) == obj
+        if isinstance(ins_id, str):
+            return True
+        if not isinstance(ins_id, tuple):
+            print("** no instance found **")
+            return False
+        # (obj, obj) == tuple
         ins_id = ins_id[0]
         if not isinstance(ins_id, str):
             # literal_eval can let other type pass and functions expect strings
@@ -284,6 +298,64 @@ class HBNBCommand(cmd.Cmd):
             return False
 
         return True
+
+    def _is_val_update(self, line, kwargs):
+        """Validates the parameters of update"""
+        all_objs = storage.all()
+        if not kwargs:
+            if not self.is_line_valid(line, "update"):
+                return False
+            args = line.split(" ", 3)
+            arg_l = len(args)
+            obj_name = f"{args[0]}.{args[1]}"
+            if obj_name not in all_objs:
+                print("** no instance found **")
+                return False
+            if arg_l == 2:  # no attributes
+                print("** attribute name missing **")
+                return False
+            if arg_l == 3:
+                print("** value missing **")
+                return False
+            return True  # All checks passed -- success
+
+        if not self._is_id_kwargs(kwargs):
+            return False
+        obj_name = f"{kwargs['class_name']}.{self._get_id(kwargs)}"
+        if obj_name not in all_objs:
+            print("** no instance found **")
+            return False
+
+        # check for attribute name and value
+        arg_l = len(kwargs['options'])
+        options = kwargs['options']
+        if not isinstance(options, tuple):  # only one options.
+            print("** attribute name missing **")
+            return False
+        elif not isinstance(options[1], dict) and arg_l == 2:
+                print("** value missing **")
+                return False
+        return True  # all checks passed -- success
+
+    def _set_attr(self, key, val, obj_name):
+        """Set the attribute of the object
+    Parameters:
+    - key: attribute name
+    - val: value to set to attribute
+    - obj_name: <class_name>.<id> of the object.
+    """
+        # do we strip here? if val = "33" ~= "'33'" the conversion to ..
+        # int below wont work?
+        value = None
+        try:
+            value = float(val) if "." in val else int(val)
+        except ValueError as e:
+            # just pass value?
+            value = self.get_update_str(val)
+        all_objs = storage.all()
+        instance = all_objs[obj_name]
+        setattr(instance, key, value)
+        instance.save()
 
 
 if __name__ == "__main__":
