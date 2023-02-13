cd66614dba6b48a3358ccdfcab91404c7c3a71a7
mubrik <mubarakg4u@gmail.com>
2023-02-13 17:48:31 +0100

    test b

--- tests/test_console.py
+++ tests/test_console.py
@@ -174,10 +174,108 @@ class TestConsole(unittest.TestCase):
             self.assertEqual("** instance id missing **\n", f.getvalue())
             f.close()
 
+    def test_destroy_basemodel_function_invalid(self):
+        """testing `BaseModel.destroy()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("BaseModel.destroy()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_destroy_place_function_invalid(self):
+        """testing `Place.destroy()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Place.destroy()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_destroy_user_function_invalid(self):
+        """testing `User.destroy()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("User.destroy()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_destroy_amenity_function_invalid(self):
+        """testing `Amenity.destroy()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Amenity.destroy()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_destroy_city_function_invalid(self):
+        """testing `City.destroy()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("City.destroy()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_destroy_state_function_invalid(self):
+        """testing `State.destroy()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("State.destroy()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_destroy_review_function_invalid(self):
+        """testing `Review.destroy()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Review.destroy()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
     def test_update_basemodel_invalid(self):
         """testing `update BaseModel` command"""
         with patch('sys.stdout', new=io.StringIO()) as f:
             HBNBCommand().onecmd("update BaseModel")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_update_basemodel_function_invalid(self):
+        """testing `BaseModel.update()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("BaseModel.update()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_update_place_function_invalid(self):
+        """testing `Place.update()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Place.update()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_update_user_function_invalid(self):
+        """testing `User.update()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("User.update()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_update_amenity_function_invalid(self):
+        """testing `Amenity.update()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Amenity.update()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_update_city_function_invalid(self):
+        """testing `City.update()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("City.update()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_update_state_function_invalid(self):
+        """testing `State.update()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("State.update()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_update_review_function_invalid(self):
+        """testing `Review.update()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Review.update()")
             self.assertEqual("** instance id missing **\n", f.getvalue())
             f.close()
 
@@ -237,6 +335,55 @@ class TestConsole(unittest.TestCase):
             self.assertEqual("[]\n", f.getvalue())
             f.close()
 
+    def test_count_basemodel_function(self):
+        """testing `BaseModel.count()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("BaseModel.count()")
+            self.assertEqual("0\n", f.getvalue())
+            f.close()
+
+    def test_count_place_function(self):
+        """testing `Place.count()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Place.count()")
+            self.assertEqual("0\n", f.getvalue())
+            f.close()
+
+    def test_count_user_function(self):
+        """testing `User.count()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("User.count()")
+            self.assertEqual("0\n", f.getvalue())
+            f.close()
+
+    def test_count_amenity_function(self):
+        """testing `Amenity.count()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Amenity.count()")
+            self.assertEqual("0\n", f.getvalue())
+            f.close()
+
+    def test_count_city_function(self):
+        """testing `City.count()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("City.count()")
+            self.assertEqual("0\n", f.getvalue())
+            f.close()
+
+    def test_count_state_function(self):
+        """testing `State.count()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("State.count()")
+            self.assertEqual("0\n", f.getvalue())
+            f.close()
+
+    def test_count_review_function(self):
+        """testing `Review.count()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Review.count()")
+            self.assertEqual("0\n", f.getvalue())
+            f.close()
+
     def test_show_invalid(self):
         """testing invalid show command"""
         with patch('sys.stdout', new=io.StringIO()) as f:
@@ -255,5 +402,54 @@ class TestConsole(unittest.TestCase):
         """testing show missing id command"""
         with patch('sys.stdout', new=io.StringIO()) as f:
             HBNBCommand().onecmd("show BaseModel")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_show_basemodel_function_invalid(self):
+        """testing `BaseModel.show()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("BaseModel.show()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_show_place_function_invalid(self):
+        """testing `Place.show()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Place.show()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_show_user_function_invalid(self):
+        """testing `User.show()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("User.show()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_show_amenity_function_invalid(self):
+        """testing `Amenity.show()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Amenity.show()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_show_city_function_invalid(self):
+        """testing `City.show()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("City.show()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_show_state_function_invalid(self):
+        """testing `State.show()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("State.show()")
+            self.assertEqual("** instance id missing **\n", f.getvalue())
+            f.close()
+
+    def test_show_review_function_invalid(self):
+        """testing `Review.show()` command"""
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("Review.show()")
             self.assertEqual("** instance id missing **\n", f.getvalue())
             f.close()
